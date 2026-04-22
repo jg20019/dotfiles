861d149eb1bef86001af4e1bf3faba398be68d22
John Gibson <jg20019@gmail.com>
2026-04-21 20:43:15 -0400

    add emacs custom-vars

---
+++ .config/emacs/custom-vars.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(paredit hydra general evil-collection evil counsel ivy-rich which-key rainbow-delimiters swiper ivy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
