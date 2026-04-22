89f277d3458bab204370749e5acda8ee2650ce26
John Gibson <jg20019@gmail.com>
2026-04-22 13:12:08 -0400

    update custom-vars

--- .config/emacs/custom-vars.el
+++ .config/emacs/custom-vars.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(paredit hydra general evil-collection evil counsel ivy-rich which-key rainbow-delimiters swiper ivy)))
+   '(cider paredit hydra general evil-collection evil counsel ivy-rich which-key rainbow-delimiters swiper ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
