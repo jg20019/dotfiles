f60ab101bab2f54e5f63f6cc34df8e22beb820b7
John Gibson <jg20019@gmail.com>
2026-05-02 18:23:52 -0400

    update custom-vars

--- .config/emacs/custom-vars.el
+++ .config/emacs/custom-vars.el
@@ -4,10 +4,32 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-bullets projectile company cider paredit hydra general evil-collection evil counsel ivy-rich which-key rainbow-delimiters swiper ivy)))
+   '(zig-mode org-bullets projectile company cider paredit hydra general evil-collection evil counsel ivy-rich which-key rainbow-delimiters swiper ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "JetBrainsMonoNL Nerd Font" :height 160))))
+ '(org-block ((t (:inherit fixed-pitch))))
+ '(org-code ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-info ((t (:foreground "dark orange"))))
+ '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo" :height 2.0 :underline nil))))
+ '(org-indent ((t (:inherit (org-hide fixed-pitch)))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#ffffff" :font "ETBembo"))))
+ '(org-link ((t (:foreground "royal blue" :underline t))))
+ '(org-meta-line ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-property-value ((t (:inherit fixed-pitch))))
+ '(org-special-keyword ((t (:inherit (font-lock-comment-face fixed-pitch)))))
+ '(org-table ((t (:inherit fixed-pitch :foreground "#83a598"))))
+ '(org-tag ((t (:inherit (shadow fixed-pitch) :weight bold :height 0.8))))
+ '(org-verbatim ((t (:inherit (shadow fixed-pitch)))))
+ '(variable-pitch ((t (:family "ETBembo" :height 180 :weight thin)))))
