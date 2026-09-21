ddcc60f3b4b48f266bc05bcb84c31f67553c6c68
John Gibson <jg20019@gmail.com>
2026-09-21 13:42:52 -0400

    use fzf with ctrl+r

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -20,6 +20,7 @@ if status is-interactive
     mise activate fish | source
     starship init fish | source
     zoxide init fish | source
+    fzf --fish | source
     set -U fish_greeting
 
     # Set nvim as man pager
